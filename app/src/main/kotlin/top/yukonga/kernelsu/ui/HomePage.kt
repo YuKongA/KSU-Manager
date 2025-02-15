73c31d3aca8ae5a6e27e98755a029eb66b91ef74
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-02-15 10:19:44 +0800

    Update UI [6/WIP] (#1)
    * --修改主页样式
    
    * --删除多余代码
    
    ---------
    
    Co-authored-by: 25748 <2574822036@qq.com>

--- app/src/main/kotlin/top/yukonga/kernelsu/ui/HomePage.kt
+++ app/src/main/kotlin/top/yukonga/kernelsu/ui/HomePage.kt
@@ -3,41 +3,55 @@ package top.yukonga.kernelsu.ui
 import android.os.Build
 import android.system.Os
 import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.CheckCircle
+import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material.icons.rounded.Link
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.chrisbanes.haze.HazeState
 import dev.chrisbanes.haze.HazeStyle
 import dev.chrisbanes.haze.HazeTint
 import dev.chrisbanes.haze.hazeEffect
 import dev.chrisbanes.haze.hazeSource
 import top.yukonga.kernelsu.R
+import top.yukonga.kernelsu.ui.component.BasicComponent
 import top.yukonga.kernelsu.ui.component.CardView
 import top.yukonga.kernelsu.ui.component.InfoText
 import top.yukonga.kernelsu.utils.getManagerVersion
 import top.yukonga.kernelsu.utils.getSELinuxStatus
-import top.yukonga.miuix.kmp.basic.BasicComponent
+import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.Icon
 import top.yukonga.miuix.kmp.basic.LazyColumn
 import top.yukonga.miuix.kmp.basic.MiuixScrollBehavior
 import top.yukonga.miuix.kmp.basic.Scaffold
+import top.yukonga.miuix.kmp.basic.Text
 import top.yukonga.miuix.kmp.basic.TopAppBar
 import top.yukonga.miuix.kmp.basic.rememberTopAppBarState
 import top.yukonga.miuix.kmp.theme.MiuixTheme
+import top.yukonga.miuix.kmp.theme.MiuixTheme.colorScheme
 import top.yukonga.miuix.kmp.utils.getWindowSize
 
 @Composable
@@ -57,9 +71,9 @@ fun HomePage(
 
     val hazeStyleTopAppBar = HazeStyle(
         blurRadius = 25.dp,
-        backgroundColor = MiuixTheme.colorScheme.background,
+        backgroundColor = colorScheme.background,
         tint = HazeTint(
-            MiuixTheme.colorScheme.background.copy(0.67f)
+            colorScheme.background.copy(0.67f)
         )
     )
 
@@ -88,36 +102,146 @@ fun HomePage(
             item {
 
                 Spacer(Modifier.height(12.dp + padding.calculateTopPadding()))
-                CardView(
-                    color = if (isSystemInDarkTheme()) Color(0xFF081831) else Color(0xFFD0E4F8),
+                Row(
+                    modifier = Modifier
+                        .height(200.dp)
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp)
+                        .padding(bottom = 12.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
-                    BasicComponent(
-                        title = stringResource(R.string.home_working) + workingMode,
-                        summary = stringResource(R.string.home_working_version, ksuVersion) + "\n" +
-                                stringResource(R.string.home_superuser_count, ksuSuperuserCount) + "\n" +
-                                stringResource(R.string.home_module_count, ksuModuleCount),
-                        leftAction = {
-                            Icon(
+                    Card(
+                        modifier = Modifier
+                            .padding(end = 12.dp)
+                            .weight(1f),
+                        color = if (isSystemInDarkTheme()) Color(0xFF1A3825) else Color(0xFFDFFAE4),
+                    ) {
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .clickable {
+                                    Toast.makeText(context.applicationContext, "TODO", Toast.LENGTH_SHORT).show()
+                                }
+                        ) {
+                            Column(
                                 modifier = Modifier
-                                    .size(40.dp)
-                                    .padding(end = 12.dp),
-                                imageVector = Icons.Rounded.CheckCircle,
-                                tint = MiuixTheme.colorScheme.onSurface,
-                                contentDescription = null
-                            )
-                        },
-                        onClick = {
-                            // TODO
-                            Toast.makeText(context.applicationContext, "TODO", Toast.LENGTH_SHORT).show()
-                        },
-                        insideMargin = PaddingValues(18.dp)
-                    )
+                                    .fillMaxSize()
+                                    .padding(14.dp)
+                            ) {
+                                Text(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    text = stringResource(R.string.home_working)+workingMode,
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.SemiBold,
+                                )
+                                Spacer(Modifier.height(8.dp))
+                                Text(
+                                    stringResource(R.string.home_working_version, ksuVersion),
+                                    fontSize = 14.sp,
+                                    fontWeight = FontWeight.Medium,
+                                )
+                            }
+                            Box(
+                                modifier = Modifier.fillMaxSize().offset(38.dp,45.dp),
+                                contentAlignment = Alignment.BottomEnd
+                            ){
+                                Icon(
+                                    modifier = Modifier.size(170.dp),
+                                    imageVector = Icons.Outlined.CheckCircle,
+                                    tint = Color(0xFF36D167),
+                                    contentDescription = null
+                                )
+
+                            }
+
+                        }
+
+                    }
+                    Column(
+                        modifier = Modifier.wrapContentHeight().weight(1f)
+                    ) {
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f),
+                            insideMargin = PaddingValues(14.dp),
+                        ) {
+                            Column{
+                                Text(
+                                    text = stringResource(R.string.superuser),
+                                    fontSize = 15.sp,
+                                    fontWeight = FontWeight.Medium,
+                                    color = colorScheme.onSurfaceVariantSummary
+                                )
+                                Row (
+                                    modifier = Modifier.fillMaxSize(),
+                                    horizontalArrangement = Arrangement.Start,
+                                    verticalAlignment = Alignment.Bottom,
+                                ){
+                                    Text(
+                                        text = ksuSuperuserCount.toString(),
+                                        fontSize = 26.sp,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                    Text(
+                                        modifier = Modifier.padding(start = 5.dp, bottom = 3.5.dp),
+                                        text = "个",
+                                        fontSize = 16.sp,
+                                        fontWeight = FontWeight.Medium,
+                                        color = colorScheme.onSurfaceVariantSummary
+                                    )
+                                }
+
+                            }
+
+                        }
+                        Spacer(Modifier.height(12.dp))
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f),
+                            insideMargin = PaddingValues(14.dp),
+                        ) {
+                            Column{
+                                Text(
+                                    text = stringResource(R.string.module),
+                                    fontSize = 15.sp,
+                                    fontWeight = FontWeight.Medium,
+                                    color = colorScheme.onSurfaceVariantSummary
+                                )
+                                Row (
+                                    modifier = Modifier.fillMaxSize(),
+                                    horizontalArrangement = Arrangement.Start,
+                                    verticalAlignment = Alignment.Bottom,
+                                ) {
+
+                                    Text(
+                                        text = ksuModuleCount.toString(),
+                                        fontSize = 26.sp,
+                                        fontWeight = FontWeight.Bold,
+                                    )
+                                    Text(
+                                        modifier = Modifier.padding(start = 5.dp, bottom = 3.5.dp),
+                                        text = "个",
+                                        fontSize = 16.sp,
+                                        fontWeight = FontWeight.Medium,
+                                        color = colorScheme.onSurfaceVariantSummary
+                                    )
+                                }
+
+                            }
+
+                        }
+                    }
                 }
+
                 CardView {
                     val uname = Os.uname()
                     val managerVersion = getManagerVersion()
                     Column(
-                        modifier = Modifier.padding(18.dp)
+                        modifier = Modifier.padding(19.dp)
                     ) {
                         InfoText(
                             title = stringResource(R.string.home_kernel),
@@ -146,7 +270,7 @@ fun HomePage(
                             Icon(
                                 modifier = Modifier.size(28.dp),
                                 imageVector = Icons.Rounded.Link,
-                                tint = MiuixTheme.colorScheme.onSurface,
+                                tint = colorScheme.onSurface,
                                 contentDescription = null
                             )
                         },
@@ -166,7 +290,7 @@ fun HomePage(
                             Icon(
                                 modifier = Modifier.size(28.dp),
                                 imageVector = Icons.Rounded.Link,
-                                tint = MiuixTheme.colorScheme.onSurface,
+                                tint = colorScheme.onSurface,
                                 contentDescription = null
                             )
                         },
